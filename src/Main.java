463c367683d4f1166fd1d5a42e66db13b30592f6
vinogradova311 <anny.amelie@gmail.com>
2022-07-09 18:31:49 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 100;
+        int refillAmount = 150;
+
+        int bonus;
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        int finalAmount = initialAmount+refillAmount+bonus;
+        System.out.println("Итоговый счет: " + finalAmount);
+        System.out.println("Бонус: " + bonus);
+    }
+}
